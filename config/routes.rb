58574941edb0ecd080242dcc01d83471fe7313c4
ColbyCheeZe <colby@colbycheeze.com>
2014-12-14 23:42:46 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
-  get 'static_pages/home'
 
   get 'static_pages/help'
 
-  root 'application#hello'
+  get 'static_pages/about'
+
+  root 'static_pages#home'
+
 end
