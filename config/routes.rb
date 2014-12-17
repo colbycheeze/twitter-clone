efd9397694a79ae149423517be88f3d0c83a7b82
ColbyCheeZe <colby@colbycheeze.com>
2014-12-16 19:40:48 -0600

    finished layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
+  get 'users/new'
 
   root 'static_pages#home'
+  get 'contact' => 'static_pages#contact'
+  get 'help' => 'static_pages#help'
+  get 'about' => 'static_pages#about'
+  get 'signup' => 'users#new'
 
 end
