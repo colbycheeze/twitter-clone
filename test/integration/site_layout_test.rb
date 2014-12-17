47c1ed2e282f942c9294cf93c815c5461672fb73
ColbyCheeZe <colby@colbycheeze.com>
2014-12-16 20:01:00 -0600

    Added additional testing, Signup link works

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
   end
 end
