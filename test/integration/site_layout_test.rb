5c876e3e097456b3aafb2fe820c7541cea4704af
ColbyCheeZe <colby@colbycheeze.com>
2014-12-17 21:32:08 -0600

    Added User model, and secure login

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -10,5 +10,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     assert_select "a[href=?]", signup_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
   end
 end
