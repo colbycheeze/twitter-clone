bfe128ffdf874a41f86d1a2272ae036662913477
ColbyCheeZe <colby@colbycheeze.com>
2014-12-20 21:13:57 -0600

    Added login form and integration test

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Login and go to show page
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   resources        :users
 
-  root             'static_pages#home'
-  get 'contact' => 'static_pages#contact'
-  get 'help'    => 'static_pages#help'
-  get 'about'   => 'static_pages#about'
-  get 'signup'  => 'users#new'
+  root                'static_pages#home'
+  get    'contact' => 'static_pages#contact'
+  get    'help'    => 'static_pages#help'
+  get    'about'   => 'static_pages#about'
+  get    'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
 end
