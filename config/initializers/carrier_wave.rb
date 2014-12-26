07d07305b2c9cdaf7da215b0bd419616b2fd4487
ColbyCheeZe <colby@colbycheeze.com>
2014-12-26 06:31:51 -0600

    Corrected S3 keys

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
-      :aws_access_key_id     => ENV['AKIAIX2GDBGULSAWEXUQ'],
-      :aws_secret_access_key => ENV['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']
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['cheeze-projects']
+    config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
 
